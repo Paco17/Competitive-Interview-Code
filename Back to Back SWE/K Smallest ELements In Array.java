8f773a66e02d35fc7fd62f08f8f2259bb7557fc4
Paco17 <326franciscorv@gmail.com>
2020-08-29 23:13:01 -0500

    Back2Back

---
+++ Back to Back SWE/K Smallest ELements In Array.java
@@ -0,0 +1,29 @@
+class Solution {
+     public List<Integer> kSmallestElements(int[] elements, int k) {
+
+     	//Brute Force
+     	/*Arrays.sort(elements);
+    	 List<Integer> lista = new ArrayList<>();
+     
+     	for(int i=0;i<k; i++){
+       	            lista.add(elements[i]);
+     	}
+     	return lista;
+	*/
+  
+ 	//PriorityQueue
+  	//Nos preocupamos por los datos primeros a salir; Osea los más pequeños 
+ 
+ 	PriorityQueue<Integer> maxHeap = new PriorityQueue<>(k, Collections.reverseOrder());
+  
+  	for(Integer element:elements){
+    	         maxHeap.add(element);  
+  
+    	        if(maxHeap.size()==k+1)
+      	                maxHeap.poll();   
+                  }
+  
+                  List<Integer> smallest = new ArrayList<>(maxHeap);
+                  return smallest;                
+    }
+}
